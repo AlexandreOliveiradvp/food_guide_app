de5284c6309c0f4ea8b92e95676b2836bdcd00c6
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2024-10-19 13:58:55 -0300

    feat: building validation form with formik and yup libs

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,36 +1,37 @@
 "use client";
 
-import React, { useState } from "react";
+//import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
 import Logo from "../../../public/assets/foodGuide_logo.png";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function Login() {
   const router = useRouter();
-  const [name, setName] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setName(e.target.value);
-  };
-  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
-  const requestLogin = () => {
-    //Instalar lib Formik+yup para lidar com validacoes de formulario
-    if (name == "Test" && password == "123456") {
-      router.push("/dashboard");
-    } else {
-      alert("Usuário Inválido!");
-    }
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      name: Yup.string().required("Preenchimeto Obrigatório"),
+      password: Yup.string().required("Preenchimento Obrigatório"),
+    }),
+    onSubmit: (values) => {
+      if (values.name == "Test" && values.password == "123456") {
+        router.push("/dashboard");
+      } else {
+        alert("Usuário Inválido!");
+      }
+    },
+  });
 
   return (
     <main className="bg-login">
       <div className="mask">
         <div className="container mx-auto flex justify-center min-h-screen items-center">
-          <div className="login-box shadow-lg pt-4">
+          <div className="login-box shadow-lg py-4">
             <div className="header flex justify-center">
               <Image src={Logo} alt="Picture of the author" className="logo" />
             </div>
@@ -38,44 +39,50 @@ export default function Login() {
               <div className="grid grid-cols-1 text-center">
                 <h1>Bem Vindo ao Food Guide</h1>
               </div>
-              <div className="grid grid-cols-1 px-7 pt-1">
-                <label className="label">
-                  Name: <br />
-                  <input
-                    name="name"
-                    type="text"
-                    className="input"
-                    onChange={handleChangeName}
-                  />
-                </label>
-              </div>
-              <div className="grid grid-cols-1 px-7 pt-3">
-                <label className="label">
-                  Password: <br />
-                  <input
-                    name="password"
-                    type="text"
-                    className="input"
-                    onChange={handleChangePassword}
-                  />
-                </label>
-              </div>
-              <div className="grid grid-cols-1 px-7 pt-6">
-                <button
-                  type="button"
-                  className="btn-default"
-                  onClick={requestLogin}
-                >
-                  Login
-                </button>
-              </div>
+              <form onSubmit={formik.handleSubmit}>
+                <div className="grid grid-cols-1 px-7 pt-1">
+                  <label className="label">
+                    Name: <br />
+                    <input
+                      type="text"
+                      className="input"
+                      {...formik.getFieldProps("name")}
+                    />
+                    {formik.touched.name && formik.errors.name ? (
+                      <span className="validation-warning">
+                        {formik.errors.name}
+                      </span>
+                    ) : null}
+                  </label>
+                </div>
+                <div className="grid grid-cols-1 px-7 pt-3">
+                  <label className="label">
+                    Password: <br />
+                    <input
+                      type="text"
+                      className="input"
+                      {...formik.getFieldProps("password")}
+                    />
+                    {formik.touched.password && formik.errors.password ? (
+                      <span className="validation-warning">
+                        {formik.errors.password}
+                      </span>
+                    ) : null}
+                  </label>
+                </div>
+                <div className="grid grid-cols-1 px-7 pt-6">
+                  <button type="submit" className="btn-default">
+                    Login
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
           <div className="toast-user">
             <p>Use to access:</p>
             <ul>
-                <li>Name: Test</li>
-                <li>Password: 123456</li>
+              <li>Name: Test</li>
+              <li>Password: 123456</li>
             </ul>
           </div>
         </div>
