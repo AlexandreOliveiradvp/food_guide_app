98436a53fc548614b932f2e7a20873c8e2bbe240
Alexandre.oliveira <alexandre.oliveira@celeritech.com.br>
2024-10-21 11:20:19 -0300

    Feat: working on integration food api

--- src/app/dashboard/consult/page.tsx
+++ src/app/dashboard/consult/page.tsx
@@ -1,4 +1,31 @@
-const consultContent = () => {
+"use client";
+import api from "@/api/api";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+const ConsultContent = () => {
+  const request = (value: string) => {
+    api
+      .get(`/recipes/complexSearch?query=${value}`)
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+  const formik = useFormik({
+    initialValues: {
+      nameProduct: "",
+    },
+    validationSchema: Yup.object({
+      nameProduct: Yup.string().required("Preenchimeto Obrigatório"),
+    }),
+    onSubmit: (values) => {
+      request(values.nameProduct);
+    },
+  });
+
   return (
     <div>
       <div className="grid grid-cols-3 py-3 z-0">
@@ -8,17 +35,35 @@ const consultContent = () => {
       </div>
       <div className="grid grid-cols-3">
         <div className="col-span-2">
-          <div className="flex w-full">
-            <div className="wrapper-input-consult">
-              <label htmlFor="" className="label">
-                Search Recipies:
-              </label>
-              <input type="text" className="input" />
+          <form onSubmit={formik.handleSubmit}>
+            <div className="flex w-full">
+              <div className="wrapper-input-consult">
+                <label htmlFor="" className="label">
+                  Search Recipies:
+                </label>
+                <input
+                  type="text"
+                  className="input"
+                  {...formik.getFieldProps("nameProduct")}
+                />
+                {formik.touched.nameProduct && formik.errors.nameProduct ? (
+                  <span className="validation-warning">
+                    {formik.errors.nameProduct}
+                  </span>
+                ) : null}
+              </div>
+              <button className="btn-default ms-2 mt-6" type="submit">
+                Search
+              </button>
             </div>
-            <button className="btn-default ms-2 mt-6">Search</button>
-          </div>
+          </form>
           <div className="w-full content-search">
-            <div className="card-search rounded-md p-2"><label htmlFor="">Search Result</label></div>
+            <div className="card-search rounded-md p-2">
+              <label>Search Result</label>
+              <div className="w-full flex justify-center">
+                <div className="loader"></div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="col-span-1 pt-6">
@@ -33,4 +78,4 @@ const consultContent = () => {
   );
 };
 
-export default consultContent;
+export default ConsultContent;
